bfcaa05e4a78b9f677560b0b9ce19d1a30f78af9
AnkurKaushik <a123ankur@yahoo.com>
2019-12-21 00:00:47 -0600

    added to about page

--- src/AboutUs.js
+++ src/AboutUs.js
@@ -1,9 +1,26 @@
 import React from 'react'
+import tp1 from './assets/tp1.JPG'
+import tp2 from './assets/tp2.JPG'
+import tp3 from './assets/tp3.JPG'
+import tp4 from './assets/tp4.JPG'
+
+
 
 const hstyle = {
     textAlign: "center",
     fontFamily: "Aclonica",
     fontSize: "65px"
+};
+const pstyle = {
+    textAlign: "center",
+    fontFamily: "Aclonica",
+    fontSize: "25px"
+};
+const pstyle2 = {
+    textAlign: "center",
+    fontFamily: "Aclonica",
+    fontSize: "25px",
+    marginTop:"20px"
 };
 
 export const AboutUs = () => (
@@ -11,5 +28,42 @@ export const AboutUs = () => (
         backgroundColor: '#e9f5f8', width: '100%',
         height: '100%'}}>
         <h2 style={hstyle}>About Us</h2>
+        <p style={pstyle}>Longhorn Smash is an organization fostered by a unified passion for the Super Smash Bros series. Its members build friendship through playing video games together, organizing social events, and of course, competing in weekly tournaments hosted by the organization’s administration team.</p> 
+
+        <p style={pstyle}>From the start of the 2019-2020 semester it became a branch of Longhorn Gaming, UT’s biggest esports club. This allowed Longhorn Smash to send their esports team to collegiate tournaments, given the opportunity to compete across the country.</p>
+        <p style={pstyle}>The primary way our group communicates is through discord, you can join below.</p>
+        <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize:'24px'
+        }}><td textAlign= 'center'> <a textAlign='center' href={"https://discord.gg/UrNNR92"}> Join our discord!</a> </td></div>
+
+        <p style={pstyle}>Below are some pictures from our previous tournaments</p>
+        <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        marginTop: '20px'
+        }}>
+            <img textAlign="center" width={.7*window.innerWidth} src={tp1} alt="first pic"></img>
+            <img textAlign="center" width={.7*window.innerWidth} src={tp2} alt="second pic"></img>
+        </div>
+        <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center'
+        }}>
+            <img textAlign="center" width={.7*window.innerWidth} src={tp3} alt="third pic"></img>
+            <img textAlign="center" width={.7*window.innerWidth} src={tp4} alt="fourth pic"></img>
+        </div>
+        <p style={pstyle2}>Find us on HornsLink</p>
+        <div style={{
+        display: 'flex',
+        alignItems: 'center',
+        justifyContent: 'center',
+        fontSize:'24px'
+        }}><td> <a textAlign='center' href={"https://utexas.campuslabs.com/engage/organization/cgc"}>Click me!</a> </td>
+        </div>
     </div>
 )
