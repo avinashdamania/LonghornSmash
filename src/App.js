47a2fa0ccfe606ed37dd9c978080b741010a1890
Ryan Menghani <menghaniryan@gmail.com>
2020-01-24 23:29:13 -0600

    Made a much better home screen I guess...

--- src/App.js
+++ src/App.js
@@ -1,18 +1,16 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route, Switch} from 'react-router-dom';
-import {Home} from './Home';
+import Home from './Home';
 import {AboutUs} from './AboutUs';
 import {PR} from './PR';
 import {NoMatch} from './NoMatch';
-import {Layout} from './components/Layout';
 import {NavigationBar} from './components/NavigationBar';
 
 class App extends Component {
   render(){
     return (
-      <React.Fragment>
+      <div>
         <NavigationBar/>
-        <Layout>
         <Router>
           <Switch>
             <Route exact path="/" component={Home}/>
@@ -21,8 +19,7 @@ class App extends Component {
             <Route component={NoMatch}/>
           </Switch>
         </Router>
-        </Layout>
-      </React.Fragment>
+      </div>
     );
   }
 }
